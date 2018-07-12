4d6208c21fc6ae935fe383a08af0eae302e9418c
wirasukma <wirasukma18@ummi.ac.id>
2018-07-12 07:58:12 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Wira Rizqulamin Sukma</title>
+</head>
+<body>
+	<h1><center>Wira Rizqulamin Sukma Deployment @ github.com</center></h1>
+</body>
+</html>
